781c6e97097c67ccae724d55b7d785d37208caf0
SpyrosGeo <xarodasimba@gmail.com>
2022-06-19 13:51:59 +0300

    completed navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ import Logo from '../assets/images/Logo.png'
 function NavBar() {
 
   return (
-  <Stack direction="row" spacing={2}>
+  <Stack direction="row" justifyContent="space-around" px="20px" sx={{gap:{sm:'122px',xs:'40px'},mt:{sm:'32px',xs:'20px'},justifyContent:'none'}}>
   <Link to="/">
     <img src={Logo} alt="logo" style={{width:'48px', height:'48px',margin:'0 20px'}} />
   </Link>

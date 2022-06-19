caa7da1edc85e233c8b1621437bcb8b932f76df5
SpyrosGeo <xarodasimba@gmail.com>
2022-06-19 13:45:52 +0300

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Stack } from '@mui/material'
+
+
+import Logo from '../assets/images/Logo.png'
+
+function NavBar() {
+
+  return (
+  <Stack direction="row" spacing={2}>
+  <Link to="/">
+    <img src={Logo} alt="logo" style={{width:'48px', height:'48px',margin:'0 20px'}} />
+  </Link>
+  <Stack direction="row" gap='40px'fontSize="24px" justifyContent="center" alignItems="flex-end">
+    <Link to='/' style={{textDecoration:'none',color:'#3A1212',borderBottom:'3px solid #FF2625'}}>Home</Link>
+    <a href='#exercises' style={{textDecoration:'none',color:'#3A1212'}}>exercises</a>
+  </Stack>
+  </Stack>
+  )
+}
+
+export default NavBar

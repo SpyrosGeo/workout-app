a9c696bde24b110f0d69779878ec85eb2670a711
SpyrosGeo <xarodasimba@gmail.com>
2022-08-21 17:10:29 +0300

    added pagination

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -7,6 +7,17 @@ import { exerciseOptions as options, fetchData} from '../Utils/fetchData'
 import ExerciseCard from './ExerciseCard'
 
 const Exercises = ({exercises,setExercices,bodyPart}) => {
+  const [currentPage,setCurrentPage] = useState(1)
+  const exercisesPerPage = 9
+  const indexOfLastExercise = currentPage * exercisesPerPage
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
+  const currentExercises = exercises.slice(indexOfFirstExercise,indexOfLastExercise)
+
+
+  const paginate = (e,value)=>{
+    setCurrentPage(value)
+    window.scrollTo({top:1800,behavior:'smooth'})
+  }
   return (
   <Box id="exercises"
   sx={{
@@ -19,10 +30,23 @@ const Exercises = ({exercises,setExercices,bodyPart}) => {
     Showing Results
   </Typography>
   <Stack direction="row" sx={{gap:{lg:'110px',xs:'50px'}}} flexWrap="wrap" justifyContent="center">
-   { exercises.map((exercise,index)=>(
+{ currentExercises?.map((exercise,index)=>(
     <ExerciseCard  key={index} exercise={exercise}/> 
   ))
   }
+  </Stack>
+  <Stack mt="100px" alignItems="center">
+  {exercises?.length > exercisesPerPage &&(
+    <Pagination 
+      color="standard" 
+      shape='rounded'
+      defaultPage={1}
+      count={Math.ceil(exercises?.length / 9)}
+      page={currentPage}
+      onChange={paginate}
+      size="large"
+      />
+  )}
   </Stack>
   </Box>
   )

00aff0ccaca8e14cefb79e83ab95d788ecda3166
SpyrosGeo <xarodasimba@gmail.com>
2022-08-21 13:50:29 +0300

    bodyPart component and scrollbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { Box } from '@mui/material'
 
 import HeroBanner from '../components/HeroBanner'
@@ -6,11 +6,13 @@ import SearchExercises from '../components/SearchExercises'
 import Exercises from '../components/Exercises'
 
 function Home() {
+  const [bodyPart,setBodyPart] = useState('all')
+  const [exercises,setExercises] = useState([])
   return (
   <Box>
     <HeroBanner />
-    <SearchExercises/>
-    <Exercises />
+    <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
+    <Exercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart}/>
   </Box>
   )
 }

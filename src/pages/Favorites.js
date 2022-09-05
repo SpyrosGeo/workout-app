d8e7b125655889b48a82ab29bd6f2ddfb03913db
SpyrosGeo <xarodasimba@gmail.com>
2022-09-05 19:23:17 +0300

    added toggle favorite functionality and base look of component

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import ExerciseCard from '../components/ExerciseCard'
 import { getLocalStorageData} from '../Utils/fetchData'
+import {Stack} from '@mui/material'
 
 function Favorites() {
     const storage = getLocalStorageData()
     console.log('storage',storage)
   return (
-    <div>Favorites</div>
+  <Stack mt='100px' direction="row" sx={{gap:{lg:'110px',xs:'50px'}}} flexWrap="wrap" justifyContent="center">
+
+  {storage?.map(exercise =>(<ExerciseCard exercise={exercise} />))}
+  </Stack>
   )
 }
 

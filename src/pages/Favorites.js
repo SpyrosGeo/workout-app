07abba74af9e8239d14cce35181e9f2cbe99fb57
SpyrosGeo <xarodasimba@gmail.com>
2022-09-05 18:16:56 +0300

    getting data from

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { getLocalStorageData} from '../Utils/fetchData'
 
 function Favorites() {
+    const storage = getLocalStorageData()
+    console.log('storage',storage)
   return (
     <div>Favorites</div>
   )
